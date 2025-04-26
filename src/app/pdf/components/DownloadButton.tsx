bdd1d42cd65e0655a094c607b8cfe56a6cfc3da0
Nahuel <salvatierra.nahuel@gmail.com>
2025-04-26 01:07:54 -0300

    Refactor DownloadButton component: update file naming convention, improve loading state handling, and replace DownloadCloud icon with SendIcon

--- src/app/pdf/components/DownloadButton.tsx
+++ src/app/pdf/components/DownloadButton.tsx
@@ -1,33 +1,56 @@
-// components/pdf/DownloadButton.tsx
-"use client";
+'use client';
 
-import { Button } from "@/components/ui/button";
-import { DownloadCloud } from "lucide-react";
-import html2pdf from "html2pdf.js";
+import html2pdf from 'html2pdf.js';
+import { SendIcon } from 'lucide-react';
+import { useState } from 'react';
+
+import { Button } from '@/components/ui/button';
 
 export default function DownloadButton({
   onDownload,
 }: {
   onDownload?: () => void;
 }) {
-  const fileName = `factura-${"asdf"}.pdf`;
+  const [loading, setLoading] = useState(false);
+  const fileName = `pedido-${new Date().toDateString()}.pdf`;
 
-  const handleClick = () => {
-    const element = document.querySelector("#catalog-pdf");
-    if (element) html2pdf(element);
-    onDownload?.();
+  const handleClick = async () => {
+    setLoading(true);
+    const element = document.querySelector('#catalog-pdf');
+
+    if (element) {
+      try {
+        const pdfBlob = await html2pdf().from(element).outputPdf('blob');
+
+        const formData = new FormData();
+        formData.append('file', pdfBlob, fileName);
+
+        await fetch('/upload', {
+          method: 'POST',
+          body: formData,
+        });
+        onDownload?.();
+      } catch (error) {
+      } finally {
+        setLoading(false);
+      }
+    }
   };
 
   return (
-    <>
-      <Button
-        onClick={handleClick}
-        variant="default"
-        className="md:max-w-[30%]"
-      >
-        Descargar {fileName}
-        <DownloadCloud />
-      </Button>
-    </>
+    <Button
+      disabled={loading}
+      onClick={handleClick}
+      variant="default"
+      className="md:max-w-[30%]"
+    >
+      {loading ? (
+        <div className="animate-spin h-5 w-5 border-2 border-t-transparent rounded-full" />
+      ) : (
+        <div className="flex truncate items-center gap-2">
+          Enviar <SendIcon />
+        </div>
+      )}
+    </Button>
   );
 }
