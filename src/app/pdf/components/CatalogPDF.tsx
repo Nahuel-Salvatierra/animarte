16733e2c5863aadbeca7198b3eb8a4c6f08ff880
Nahuel <salvatierra.nahuel@gmail.com>
2025-04-26 01:07:47 -0300

    Refactor CatalogPDF component: reorder imports for consistency and replace Image with SafeImage

--- src/app/pdf/components/CatalogPDF.tsx
+++ src/app/pdf/components/CatalogPDF.tsx
@@ -1,6 +1,9 @@
-"use client";
-import { CartItem } from "@/store/useCartStore";
-import Image from "next/image";
+'use client';
+
+import Image from 'next/image';
+
+import SafeImage from '@/components/SafeImage';
+import { CartItem } from '@/store/useCartStore';
 
 const CatalogPDF = ({ cartItems }: { cartItems: CartItem[] }) => {
   return (
@@ -11,13 +14,13 @@ const CatalogPDF = ({ cartItems }: { cartItems: CartItem[] }) => {
             <div
               key={item.reference.id}
               style={{
-                display: "flex",
-                flexDirection: "row",
-                alignItems: "flex-start",
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'flex-start',
               }}
             >
               {
-                <Image
+                <SafeImage
                   style={{ width: 70, height: 70 }}
                   src={item.reference.images[0]}
                   width={70}
