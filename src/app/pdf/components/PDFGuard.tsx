8e3418fa10a5ede18be137dca34e929a0cee87bb
Nahuel <salvatierra.nahuel@gmail.com>
2025-04-29 20:40:03 -0300

    Refactor PDFGuard: add cartItems length check and update toast logic

--- src/app/pdf/components/PDFGuard.tsx
+++ src/app/pdf/components/PDFGuard.tsx
@@ -5,14 +5,24 @@ import { useRef } from 'react';
 import { toast } from 'react-toastify';
 
 import { useName } from '@/store/useCart';
+import { useCartStore } from '@/store/useCartStore';
 
 export default function PDFGuard({ children }: { children: React.ReactNode }) {
   const name = useName((state) => state.getName());
+  const cartItems = useCartStore((state) => state.cartItems);
+
+  const cartItemsLength = Object.keys(cartItems).length;
+
   const hasShownToast = useRef(false);
   const router = useRouter();
 
   if (name === '' && !hasShownToast.current) {
     toast.error('Por favor, completa el nombre de la tienda');
+    hasShownToast.current = true;
+    router.replace('/');
+  }
+
+  if (cartItemsLength === 0 && !hasShownToast.current) {
     hasShownToast.current = true;
     router.replace('/');
   }
