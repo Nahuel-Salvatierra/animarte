7052665d8d1bc625abfcff53f1bc1d6feaf648ca
Nahuel <salvatierra.nahuel@gmail.com>
2025-04-28 21:11:23 -0300

    Refactor NavBar component: reorganize imports and improve readability

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,18 +1,20 @@
-"use client";
+'use client';
 
-import * as React from "react";
+import Image from 'next/image';
+import * as React from 'react';
+import { useState } from 'react';
 
-import { Button } from "@/components/ui/button";
+import Cart from '../Cart/Cart';
+import { Badge } from '../ui/badge';
+import { Sheet, SheetTrigger } from '../ui/sheet';
+import Dropdown from './Dropdown';
 
-import { Badge } from "../ui/badge";
-import { useCartStore } from "@/store/useCartStore";
-import { Sheet, SheetTrigger } from "../ui/sheet";
-import Cart from "../Cart/Cart";
-import Dropdown from "./Dropdown";
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import { useCartStore } from '@/store/useCartStore';
 
 export function NavBar() {
   const cartItems = useCartStore((state) => state.cartItems);
+  const [open, setOpen] = useState(true);
 
   const cartCount = Object.keys(cartItems).length;
 
@@ -28,19 +30,22 @@ export function NavBar() {
               className="text-secondary inline-flex rounded-md px-3
                       hover:text-primary hover:bg-secondary"
             >
-              <Button className="flex items-center  cursor-pointer">
+              <Button
+                onClick={() => setOpen(true)}
+                className="flex items-center  cursor-pointer"
+              >
                 <p className="tracking-wide">PEDIR</p>
                 <Badge className="rounded-full h-4 w-4" variant="destructive">
                   {cartCount}
                 </Badge>
               </Button>
             </SheetTrigger>
-            <Cart />
+            <Cart open={open} setOpen={() => setOpen((prev) => !prev)} />
           </Sheet>
         </div>
         <Image
           alt="animarte logo"
-          src={"/animarte_logo.png"}
+          src={'/animarte_logo.png'}
           className="hidden sm:block justify-self-end"
           width={140}
           height={50}
