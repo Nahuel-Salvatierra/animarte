05c69f0de06f1dbf233e58acf7b7d12103db2b50
Nahuel <salvatierra.nahuel@gmail.com>
2025-04-28 21:11:15 -0300

    Refactor Cart component: enhance form handling and improve structure

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,20 +1,27 @@
-import { Button } from "../ui/button";
+import { useRouter } from 'next/navigation';
+import { FormEvent, useRef, useState } from 'react';
+import { toast } from 'react-toastify';
 
-import {
-  SheetClose,
-  SheetContent,
-  SheetDescription,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-} from "../ui/sheet";
-import { CartItem, useCartStore } from "@/store/useCartStore";
-import { useRouter } from "next/navigation";
-import CartCard from "./CartCard";
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+import { Label } from '../ui/label';
+import { SheetContent, SheetTitle } from '../ui/sheet';
+import CartCard from './CartCard';
 
-export default function Cart() {
+import { useName } from '@/store/useCart';
+import { CartItem, useCartStore } from '@/store/useCartStore';
+
+export default function Cart({
+  open,
+  setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) {
   const cartItems = useCartStore((state) => state.cartItems);
   const onAmountChange = useCartStore((state) => state.amountChange);
+  const setName = useName((state) => state.setName);
+  const nameRef = useRef<HTMLInputElement>(null);
 
   const router = useRouter();
 
@@ -32,30 +39,56 @@ export default function Cart() {
     ...value,
   }));
 
+  const handleOnSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get('name')?.toString() ?? '';
+
+    if (name === '') {
+      toast.warning('Por favor, completa el nombre de la tienda');
+      if (nameRef.current) {
+        nameRef.current.focus();
+      }
+      return;
+    } else {
+      setOpen(false);
+      router.push('/pdf');
+    }
+  };
+
+  const handleChange = (e: FormEvent<HTMLInputElement>) => {
+    const { value } = e.currentTarget;
+    setName(value);
+  };
+
   return (
-    <SheetContent>
-      <SheetHeader className="pb-0">
-        <SheetTitle>Solicitud de pedido</SheetTitle>
-        <SheetDescription>
-          El peidido se descargara en formato PDF.
-        </SheetDescription>
-      </SheetHeader>
-      <div className="overflow-y-auto p-2 gap-2 flex flex-row flex-wrap">
-        {cartItemsIterable.map((item) => (
-          <CartCard
-            key={item.id}
-            item={item}
-            handleAmountChange={handleAmountChange}
-          />
-        ))}
-      </div>
-      <SheetFooter>
-        <SheetClose asChild>
-          <Button type="button" onClick={() => router.push("/pdf")}>
-            Enviar
-          </Button>
-        </SheetClose>
-      </SheetFooter>
-    </SheetContent>
+    <>
+      {open && (
+        <SheetContent>
+          <form
+            onSubmit={(e) => handleOnSubmit(e)}
+            className="overflow-y-auto pt-3 pl-4 pr-4 gap-2 flex flex-row flex-wrap"
+          >
+            <SheetTitle>Solicitud de pedido</SheetTitle>
+            Por favor, completa los campos a continuación para que podamos
+            procesar tu pedido.
+            <Label className="text-sm text-muted-foreground">
+              Nombre/Tienda
+            </Label>
+            <Input ref={nameRef} name="name" onChange={handleChange} />
+            {cartItemsIterable.map((item) => (
+              <CartCard
+                key={item.id}
+                item={item}
+                handleAmountChange={handleAmountChange}
+              />
+            ))}
+            <Button className="w-full mb-2" type="submit">
+              Enviar
+            </Button>
+          </form>
+        </SheetContent>
+      )}
+    </>
   );
 }
