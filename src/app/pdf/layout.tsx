48883d221aeb313a91b0196f071a6d7c9765dffe
Nahuel <salvatierra.nahuel@gmail.com>
2025-04-28 21:11:10 -0300

    Fix import path for PDFGuard component in Layout

--- src/app/pdf/layout.tsx
+++ src/app/pdf/layout.tsx
@@ -1,4 +1,4 @@
-import PDFGuard from './components/pdfLayout';
+import PDFGuard from './components/PDFGuard';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <PDFGuard>{children}</PDFGuard>;
