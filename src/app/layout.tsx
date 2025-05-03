146037988954d8a86811bf7f7b68cc85ef082981
Nahuel <salvatierra.nahuel@gmail.com>
2025-05-03 20:26:16 -0300

    Remove unused imports from layout.tsx to clean up the code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { Instagram, MessageCircleMore } from 'lucide-react';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import { ToastContainer } from 'react-toastify';
