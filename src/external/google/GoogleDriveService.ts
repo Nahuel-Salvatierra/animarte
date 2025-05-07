794c379fdf804d0d6d1d6b8d363241504db8ab61
Nahuel <salvatierra.nahuel@gmail.com>
2025-05-07 20:42:01 -0300

    Add searchByName method to GoogleDriveService for file searching functionality

--- src/external/google/GoogleDriveService.ts
+++ src/external/google/GoogleDriveService.ts
@@ -16,7 +16,7 @@ export class GoogleDriveService {
   constructor() {
     if (!process.env.GOOGLE_API_KEY || !process.env.GOOGLE_DRIVE_FOLDER_ID) {
       throw new Error(
-        'Faltan variables de entorno (GOOGLE_API_KEY o GOOGLE_DRIVE_FOLDER_ID)',
+        'Missing environment variables (GOOGLE_API_KEY o GOOGLE_DRIVE_FOLDER_ID)',
       );
     }
 
@@ -51,15 +51,51 @@ export class GoogleDriveService {
     }
   }
 
+  async searchByName(fileName: string): Promise<{
+    files: DriveFile[];
+  }> {
+    const folderId = process.env.GOOGLE_DRIVE_SEARCH_FOLDER_ID;
+    const googleSearchApiKey = process.env.GOOGLE_DRIVE_SEARCH_API_KEY;
+    const q = `'${folderId}' in parents and name contains '${fileName}' and trashed = false`;
+
+    try {
+      const response = await this.drive.files.list({
+        q,
+        auth: googleSearchApiKey,
+        orderBy: 'name',
+        fields: 'files(id, name, mimeType, webContentLink, thumbnailLink)',
+      });
+
+      return {
+        files:
+          response.data.files?.map((file) => ({
+            id: file.id || '',
+            name: file.name || '',
+            image: file.thumbnailLink || '/placeholder-image.png',
+            mimeType: file.mimeType || '',
+          })) || [],
+      };
+    } catch (error) {
+      console.error('Error al buscar archivos:', error || error);
+      throw new Error('Error al buscar archivos en Google Drive');
+    }
+  }
+
   async listFiles(
     pageToken?: string,
     pageSize: number = 100,
     category?: string,
+    fileName?: string,
   ): Promise<{
     files: DriveFile[];
     nextPageToken?: string;
   }> {
     const folderId = this.getFolderId(category);
+
+    if (fileName) {
+      const files = await this.searchByName(fileName);
+      return files;
+    }
 
     try {
       const response = await this.drive.files.list({
