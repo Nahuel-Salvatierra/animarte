0814897c9edc0734e282f3540181a40fb681434e
Nahuel <salvatierra.nahuel@gmail.com>
2025-05-07 21:44:43 -0300

    Refactor SearchInput component for improved styling and functionality

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,8 @@
 'use client';
 
+import { LucidePenSquare, SearchIcon } from 'lucide-react';
 import { useRouter, useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useDebounce } from 'use-debounce';
 
 import { Input } from './ui/input';
@@ -11,6 +12,7 @@ export default function SearchInput() {
   const searchParams = useSearchParams();
   const [query, setQuery] = useState(searchParams.get('query') || '');
   const [debouncedQuery] = useDebounce(query, 500);
+  const ref = useRef<HTMLInputElement>(null);
 
   const updateQueryParams = (value: string) => {
     const params = new URLSearchParams(searchParams.toString());
@@ -27,13 +29,22 @@ export default function SearchInput() {
   }, [debouncedQuery]);
 
   return (
-    <div className="flex bg-secondary items-center gap-2">
+    <div className="flex bg-secondary justify-start h-7 transition-all duration-0 rounded-md items-center">
+      <SearchIcon
+        className="w-7 hover:scale-110 transition-all ease-in-out h-4 text-primary"
+        onClick={() => ref.current?.focus()}
+      />
       <Input
+        ref={ref}
         type="text"
-        placeholder="Buscar imágenes..."
+        placeholder="Buscar..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="border rounded px-4 py-2 w-full"
+        className="border-none w-0 px-0 max-w-screen absolute top-14
+                  opacity-0
+                  transition-all duration-100 ease-in-out
+                  focus-visible:ring-2 focus:px-2 focus:opacity-100 focus:visible focus:w-[96%] h-7
+                  sm:relative sm:top-0 right-2 sm:focus-visible:ring-0"
       />
     </div>
   );
